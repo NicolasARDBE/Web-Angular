c25dd98a7d61790493f5b5b1010c3267979e2791
Jairo Andrés <133978172+Jairo-Andres@users.noreply.github.com>
2024-04-16 15:45:38 -0500

    fincas

--- src/app/components/fincas/fincas.component.ts
+++ src/app/components/fincas/fincas.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
+import { Finca } from '../../models/finca';
+import { FincaService } from '../../services/finca.service';
 
 @Component({
   selector: 'app-fincas',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule], // Añade FormsModule aquí
   templateUrl: './fincas.component.html',
-  styleUrl: './fincas.component.css'
+  styleUrls: ['./fincas.component.css']
 })
-export class FincasComponent {
+export class FincaComponent implements OnInit {
+  fincas: Finca[] = [];
+  finca: Finca = new Finca();
+  editing: boolean = false;
 
+  constructor(private fincaService: FincaService) {}
+
+  ngOnInit() {
+    this.getFincas();
+  }
+
+  resetFinca() {
+    this.finca = new Finca(); // Crea una nueva instancia de Finca
+    this.editing = false;    // Actualiza el estado de edición
+  }
+
+  getFincas() {
+    this.fincaService.getAllFincas().then(data => {
+      this.fincas = data;
+    }).catch(error => console.error('Error fetching fincas:', error));
+  }
+
+  saveFinca() {
+    this.fincaService.saveFinca(this.finca).then(() => {
+      this.getFincas(); // Refresh the list
+      this.finca = new Finca(); // Reset the form
+      this.editing = false;
+    }).catch(error => console.error('Error saving finca:', error));
+  }
+
+  editFinca(finca: Finca) {
+    this.finca = { ...finca };
+    this.editing = true;
+  }
+
+  deleteFinca(id: number | null | undefined) {
+    if (id !== null && id !== undefined) { // Asegura que id no sea null ni undefined
+      this.fincaService.deleteFinca(id).then(() => {
+        this.getFincas(); // Refresca la lista
+      }).catch(error => console.error('Error deleting finca:', error));
+    }
+  }
+  
 }
