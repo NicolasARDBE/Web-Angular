f7825ebaceadb3b23b934c7cd86d1bd784d5e98f
Jairo Andrés <133978172+Jairo-Andres@users.noreply.github.com>
2024-04-16 12:05:10 -0500

    up

--- src/app/components/arrendador/arrendador/arrendador.component.ts
+++ src/app/components/arrendador/arrendador/arrendador.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Arrendador } from '../../../models/Arrendador';
+import { Arrendador } from '../../../models/arrendador';
 
 @Component({
   selector: 'app-arrendador',
