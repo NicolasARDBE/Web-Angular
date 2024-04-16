96a7aae59fc46e9bc35b4fcb41adcf4905a8a53e
Jairo Andrés <133978172+Jairo-Andres@users.noreply.github.com>
2024-04-16 16:32:34 -0500

    fincas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router,RouterModule,RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ArrendadorService } from './services/arrendador.service';
 import { ListArrendadorComponent } from "./components/arrendador/list-arrendador/list-arrendador.component";
 import { ActualizarArrendadorComponent } from "./components/arrendador/actualizar-arrendador/actualizar-arrendador.component";
 import { InsertarArrendadorComponent } from "./components/arrendador/insertar-arrendador/insertar-arrendador.component";
 import { EliminarArrendadorComponent } from "./components/arrendador/eliminar-arrendador/eliminar-arrendador.component";
+import { FincaComponent } from './components/fincas/fincas.component';
+import { routes } from './app.routes'; // Asume que tienes un archivo app.routes.ts que exporta tus rutas
+
+
+
 //import { DatosestudiantesComponent } from "./components/datosestudiantes/datosestudiantes.component";
 //import { DatosclaseComponent } from "./components/datosclase/datosclase.component";
 
@@ -14,10 +19,13 @@ import { EliminarArrendadorComponent } from "./components/arrendador/eliminar-ar
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, CommonModule, ListArrendadorComponent, ActualizarArrendadorComponent, InsertarArrendadorComponent, EliminarArrendadorComponent]
+    imports: [RouterOutlet, CommonModule, ListArrendadorComponent, ActualizarArrendadorComponent, InsertarArrendadorComponent, EliminarArrendadorComponent, FincaComponent, RouterModule ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'WebAngular';
-  constructor(private arrendadorService: ArrendadorService){}
-  ngOnInit(): void {}
+  constructor(private router: Router){}
+  ngOnInit(): void {
+    this.router.resetConfig(routes); // Esto configura tus rutas
+  }
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ListArrendadorComponent } from "./components/arrendador/list-arrendador/list-arrendador.component";
 import { ActualizarArrendadorComponent } from "./components/arrendador/actualizar-arrendador/actualizar-arrendador.component";
+import { FincaComponent } from './components/fincas/fincas.component';
 
 export const routes: Routes = [
   {
@@ -12,5 +13,13 @@ export const routes: Routes = [
     path: 'actualizar-arrendador/:id',
     component: ActualizarArrendadorComponent,
     title: 'Actualizar Arrendador'
+  },
+
+  {
+    path: 'fincas',
+    component: FincaComponent,
+    title: 'Listar Fincas'
   }
+
+  
 ];
